3533806c4591146e8c7d6c20ff42a295e9d17b09
Tribyt-main <bafrilanse@gmail.com>
2025-01-07 06:46:55 +0300

    Знакомство с Django

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+class Index2(TemplateView):
+    template_name = 'index2.html'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.task2.views import func_temp, class_temp
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', func_temp),
+    path('class/', class_temp.as_view())
+    # path('index/', TemplateView.as_view(template_name='index2.html')) #FOR RUN CLASS
 ]
